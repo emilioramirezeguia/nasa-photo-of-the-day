c181d2399a33cd907e6cfdd4acbb6271abd6c25d
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-06-10 21:26:34 -0700

    Fetch data from nasa api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { BASE_URL, API_KEY  } from "./constants/secrets.js";
 import "./App.css";
 
 function App() {
+  const [nasa, setNasa] = useState([]);
+
+  useEffect( function() {
+    axios.get(`${BASE_URL}api_key=${API_KEY}`)
+    .then(response => {
+      setNasa(response.data);
+    })
+    .catch(error => {
+      console.log("Error: ", error);
+    })
+  }, []);
+
   return (
     <div className="App">
       <p>
